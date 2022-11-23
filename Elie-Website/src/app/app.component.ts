6255b89319a5798d0919083c8367ff4abf8e5002
Elie-X <eliebouchergendron@gmail.com>
2022-11-23 03:32:38 -0500

    Sync + fix pour API Call mais fonctionne que sur une liste

--- Elie-Website/src/app/app.component.ts
+++ Elie-Website/src/app/app.component.ts
@@ -1,22 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ConnectableObservable } from 'rxjs';
 import { MoviesApiService } from './services/movies-api.service';
+import { Observable, map } from 'rxjs';
+import { Movie } from './movie';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Elie-Website';
 
-  movieData:any[] = [];
-  constructor(private movies_api:MoviesApiService) {}
+export class AppComponent {
+  title = 'Elie-Website';/*
+  movieData: any;*/
+
+  //posts: Movie | undefined;
+  data: any;
+
+  constructor(private movies_api:MoviesApiService) {
+  }
 
   ngOnInit() {
-    this.movies_api.movie_details.subscribe((data: any)=>{
-      this.movieData.push(data)
-      console.log(this.movieData)
+    this.movies_api.movie_details.subscribe((data: any) => {
+      //this.posts = data;
+      this.data = data;
     });
   }
 }
--- Elie-Website/src/app/services/movies-api.service.ts
+++ Elie-Website/src/app/services/movies-api.service.ts
@@ -1,21 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
+import { Movie } from '../movie';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesApiService {
 
-  constructor(private http:HttpClient) { }
-  ids:number[] = [53400, 67713, 20529, 13707];
+  //ids:number[] = [53400, 67713, 20529, 13707];
 
   movie_details = new Observable((observer) =>{
-    console.log("Enter observer")
-    for (let id of this.ids) {
+    //observer.next(this.http.get<Movie>('https://api.themoviedb.org/3/movie/53400?api_key=5c07469ee5702e41091c72f07e7c4d9a'))
+    observer.next(this.http.get('https://jsonplaceholder.typicode.com/posts'))
+    /*for (let id of this.ids) {
       let movie = this.http.get('https://api.themoviedb.org/3/movie/' + id + '?api_key=5c07469ee5702e41091c72f07e7c4d9a')
       observer.next(movie)
-    }
+    }*/
   })
-}
 
+  
+  
+
+  constructor(private http: HttpClient) {
+
+  }
+}
