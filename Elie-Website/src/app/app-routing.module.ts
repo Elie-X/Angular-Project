05e1a05c564a2b39fb08e55dc7747678d4628104
Elie-X <eliebouchergendron@gmail.com>
2022-11-25 11:18:47 -0500

    Ajout routing pour autres pages

--- Elie-Website/src/app/app-routing.module.ts
+++ Elie-Website/src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FavoritesComponent } from './components/favorites/favorites.component';
 import { HomeComponent } from './components/home/home.component';
-import { VideoPlayerComponent } from './components/video-player/video-player.component';
+import { VideoListComponent } from './components/video-list/video-list.component';
+import { VideoPageComponent } from './components/video-page/video-page.component';
 
 //Add paths to const.
 const routes: Routes = [
   {
     path: '', component: HomeComponent
   }, {
-    path: 'VideoPlayer', component: VideoPlayerComponent
+    path: 'VideoPlayer', component: VideoPageComponent
+  }, {
+    path: 'Favorites', component: FavoritesComponent
+  }, {
+    path: 'VideoList', component: VideoListComponent
   }
 ];
 
