0806f5a122eab6c5ec4b927d4672797271bf207e
Elie-X <eliebouchergendron@gmail.com>
2022-11-25 12:25:36 -0500

    Ajout routing Paramètres

--- Elie-Website/src/app/app-routing.module.ts
+++ Elie-Website/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FavoritesComponent } from './components/favorites/favorites.component';
 import { HomeComponent } from './components/home/home.component';
+import { ParametersComponent } from './components/parameters/parameters.component';
 import { VideoListComponent } from './components/video-list/video-list.component';
 import { VideoPageComponent } from './components/video-page/video-page.component';
 
@@ -15,6 +16,8 @@ const routes: Routes = [
     path: 'Favorites', component: FavoritesComponent
   }, {
     path: 'VideoList', component: VideoListComponent
+  }, {
+    path: 'Parameters', component: ParametersComponent
   }
 ];
 
